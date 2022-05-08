cdee9a85458da28418d070cc5635b7163791884a
yuya.yokoyama <yuya.yokoyama@monocrea.co.jp>
2022-05-09 00:19:35 +0900

    カルーセル修正

---
+++ handmade-store-front/components/home/Calousel.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-carousel height="200">
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      />
+    </v-carousel>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class Calousel extends Vue {
+  items = [
+    {
+      src: '/img/no-image.png',
+    },
+    {
+      src: '/img/no-image.png',
+    },
+    {
+      src: '/img/no-image.png',
+    },
+    {
+      src: '/img/no-image.png',
+    },
+  ];
+}
+</script>
